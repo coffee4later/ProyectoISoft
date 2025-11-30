e5fa96d295c6f69d7d747f1bbd17cc08dd9acc7c
Lufture <20030065@itcelaya.edu.mx>
2025-11-30 15:55:31 -0600

    Se agregan conexiones a base de datos con render y el use de env file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import server from './server.ts';
+import server from './server';
 
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import express from 'express';
-import router from './router.ts';
+import router from './router';
+import db from './config/db';
 
+// COnectar db
+async function conectarDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log('Base de datos conectada');
+    } catch (error) {
+        console.log(error);
+        console.log('Hubo un error al conectar la base de datos');
+
+    }
+}
+conectarDB();
 const server = express();
 
 server.use('/productos', router);
